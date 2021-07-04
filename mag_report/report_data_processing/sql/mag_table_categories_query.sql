b2ed1ce82078882af00b56485de3c3b3c97618bd
Bianca Kramer <bianca.kramer@gmail.com>
2021-07-04 09:42:29 +0200

    Add abstract as variable

--- mag_report/report_data_processing/sql/mag_table_categories_query.sql
+++ mag_report/report_data_processing/sql/mag_table_categories_query.sql
@@ -19,7 +19,12 @@ WITH truth_table AS (
             ELSE FALSE
         END
         as has_affiliation_string,
-         CASE
+        CASE
+            WHEN abstract is not null THEN TRUE
+            ELSE FALSE
+        END
+        as has_abstract,
+        CASE
             WHEN CitationCount > 0 THEN TRUE
             ELSE FALSE
         END
@@ -50,6 +55,7 @@ SELECT
     COUNTIF(has_affiliation_id and Doi is not null) as dois_with_affiliation_ids,
     COUNTIF(has_gridid and Doi is not null) as dois_with_grids,
     COUNTIF(has_affiliation_string and Doi is not null) as dois_with_affiliation_strings,
+    COUNTIF(has_abstract and Doi is not null) as dois_with_abstract,
     COUNTIF(has_citations and Doi is not null) as dois_with_citations,
     COUNTIF(has_references and Doi is not null) as dois_with_references,
     COUNTIF(num_fields > 1 and Doi is not null) as dois_with_more_than_one_field,
@@ -58,6 +64,7 @@ SELECT
     COUNTIF(has_affiliation_id) as objects_with_affiliation_ids,
     COUNTIF(has_gridid) as objects_with_grids,
     COUNTIF(has_affiliation_string) as objects_with_affiliation_strings,
+    COUNTIF(has_abstract) as objects_with_abstract,
     COUNTIF(has_citations) as objects_with_citations,
     COUNTIF(has_references) as objects_with_references,
     COUNTIF(num_fields > 1) as objects_with_more_than_one_field,
